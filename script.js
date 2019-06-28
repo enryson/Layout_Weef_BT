f6806238e7ed1586b7be3297e908e2fea1d96c32
enryson <enryson@gmail.com>
2019-06-28 17:43:20 -0300

    FIX
    Better way to fetch data on the carroucel

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
+    fetchCarroucel();
+});
 
+function fetchCarroucel() {
     fetch('https://teste.teste404.com.br/weef/sliders.json')
         .then(function(response) {
             return response.json();
         })
         .then(function(myJson) {
+
+            var activeVar = "";
+
             for (var i = 0; i < myJson.length; i++) {
-                console.log(myJson[i].image.src);
-                $(".carousel-inner").append($('<div class="carousel-item">' +
-                    '<img class="d-block w-100" src="/img/bg.png" alt="First slide">' +
+                if (i == 0)
+                    activeVar = " active ";
+                else
+                    activeVar = "";
+                $(".carousel-inner").append($('<div class="carousel-item' + activeVar + '">' +
+                    '<img class="d-block w-100" src="/img/bg.png">' +
                     '<div class="carousel-content">' +
                     '<h1>' + myJson[i].title + '</h1>' +
                     '<h4>' + myJson[i].subtitle + '</h4>' +
-                    '<p>' + myJson[i].excerpt + '</p>'));
-
+                    '<p>' + myJson[i].excerpt + '</p>' + "</div> </div>"));
             }
         });
-});
+}
